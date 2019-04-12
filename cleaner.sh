d4042d52847be5b851d93c1ed28352f36f41e3b5
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-04-12 18:31:52 +0500

    vasl engine

--- cleaner.sh
+++ cleaner.sh
@@ -1,3 +1,12 @@
+#***************************************************************************************#
+#----------------------- EULA LICENSE AGREEMENT NOTICE ---------------------------------#
+#1. This software uses EULA based software agreement that grants users rights to use for#
+#any purpose, modify and redistribute creative works about this perl software.          #
+#2. This software comes in "is-as" warranty, author "Haroon Awan" take no responsbility #
+#what you do with by/this software. Your free to use this software as it is for any     #
+#purpose that suites as long as it is not related to crime.                             #
+#***************************************************************************************#
+
 #!/bin/bash
 sed 's/,//g' subdomains.txt > s1
 sed 's/"//g' s1 > s2
