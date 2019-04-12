e8cb4bafd876ed145d27e23e812ab68302fe2d74
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-04-12 14:41:49 +0500

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -2,7 +2,8 @@
 sed 's/,//g' subdomains.txt > s1
 sed 's/"//g' s1 > s2
 sed 's/.$//g' s2 > s3
-echo "Enter Domain in dot format: "
+echo "[ + ] Cleaning and automatic adjusting data "
+echo -n "[ + ] Enter Domain again in dot format now: "
 read web; awk -v v1=$web '{print $0 v1}' s2 > s3
 sed 's/\.\././g' s3 > s4
 tail -n+5 s4 > s5
@@ -14,3 +15,4 @@ rm s3
 rm s4
 rm s5
 rm s6
+rm subdomains.txt
