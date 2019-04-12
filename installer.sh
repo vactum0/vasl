28ef4b27b4587d335855db6b6b3ab2e773587654
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-04-12 14:42:39 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir /root/Downloads/vasl
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/python:~/root/Downloads/vasl/
-alias myscript=~/root/Downloads/vasl/
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/perl:/usr/sbin/python:/usr/bin/python:~/root/Downloads/vasl-master/
+alias myscript=~/root/Downloads/vasl-master/
 cd /usr/bin
-ln -fsT /root/Downloads/vasl/vasl.py /usr/bin/vasl
-chmod u+x vasl.py
-chmod u+x cleaner.sh
+ln -fsT /root/Downloads/vasl-master/vasl.py /usr/bin/vasl
+chmod u+x /root/Downloads/vasl-master/vasl.py
+chmod u+x /root/Downloads/vasl-master/cleaner.sh
 chmod u+x /usr/bin/vasl
+cd /root/
 echo "Installed!"
 echo "VASL can be called anywhere from system"
